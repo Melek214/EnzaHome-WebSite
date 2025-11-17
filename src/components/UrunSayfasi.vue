5973ccee69933e7a770be71bf95e0a8fd10b9d55
Melek Yazıcı <melek@Melek-MacBook-Air.local>
2025-11-17 23:38:49 +0300

    feat:Yeni 'UrunSayfasi' bileşik iskeleti eklendi

---
+++ src/components/UrunSayfasi.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="product-page-container">
+    
+    <h2> Ürün Sayfası</h2>
+
+    <div class="product-main-content">
+      
+      <div class="product-image-gallery">
+        </div>
+      
+      <div class="product-details">
+        </div>
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+/* Şimdilik sadece ana taşıyıcıya padding ekleyelim */
+.product-page-container {
+  padding: 20px 50px;
+  max-width: 1400px;
+  margin: 0 auto;
+}
+
+.product-main-content {
+  display: flex;
+  gap: 40px;
+  border-top: 1px solid #eee; /* Başlık altındaki çizgiyi koyalım */
+  padding-top: 20px;
+}
+</style>
